9354f4f53cb352d45fe7903c248618291b7ecef4
joulsCarvajal <juliancarvajaldiaz1992@outlook.com>
2023-01-18 13:43:55 -0500

    FirstCommit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joulsCarvajal/claseModulesGo
+
+go 1.19
---
+++ utils.go
@@ -0,0 +1,9 @@
+// Ya no usamos el Package Main porque ya mi programa funcionará como dependencia para proyectos externos
+package utils
+
+import "fmt"
+
+// Primera letra en mayus como PUBLICA
+func HelloWolrd() {
+	fmt.Println("Hello World friends!!!")
+}
